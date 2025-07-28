27108b2e30ab5994d154f2514a5997e360aced7f
Samudro Sinha <samudrosinha@Samudros-MacBook-Air.local>
2025-07-28 16:50:14 +0530

    parse request line

--- app/main.py
+++ app/main.py
@@ -1,16 +1,45 @@
 import socket  # noqa: F401
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def decode_data(data):
+    print("data:", data)
+    return data.decode("utf-8") if data else None
 
-    # Uncomment this to pass the first stage
-    #
+
+def extract_request_line(request):
+    print("request:", request)
+    request_line = request.split("\r\n")[0]
+    print("request_line:", request_line)
+    return request_line if request_line else None
+
+
+def step_3_check(request):
+    request_line = extract_request_line(request)
+    if request_line:
+        request_list = request_line.split()
+        if request_list[0] == "GET" and request_list[1] == "/":
+            return b"""HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"""
+        else:
+            return b"""HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n"""
+    else:
+        return b"""HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\n"""
+
+
+def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, addr = server_socket.accept() 
-    message = b"""HTTP/1.1 200 OK\r\n\r\n"""
-    conn.send(message)
+    print("Server is up and listening on port 4221...")
+
+    while True:
+        conn, addr = server_socket.accept() 
+        print(f"Connected to {addr}.")
+        buffer_size = 1024
+        data = conn.recv(buffer_size)
+
+        request = decode_data(data)
+        response = step_3_check(request)
+        conn.sendall(response)
+        conn.close()
+        print(f"Connection to {addr} closed.")
 
 
 if __name__ == "__main__":
